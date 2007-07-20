7bbb33c17a70803ea96a05ef73dee599f6b702e7
Ricardo Signes <rjbs@cpan.org>
2007-07-20 00:58:32 +0000

    r32128@knight:  rjbs | 2007-07-19 20:55:27 -0400  perltidy; fix crlf

--- lib/Email/Reply.pm
+++ lib/Email/Reply.pm
@@ -12,6 +12,7 @@ use vars qw[$VERSION $CLASS @EXPORT];
 $VERSION = '1.202';
 $CLASS   = __PACKAGE__;
 @EXPORT  = qw[reply];
+my $CRLF = "\x0d\x0a";
 
 # Want to subclass and still use the functional interface?
 # That's cool, just add these lines to your package:
@@ -22,148 +23,145 @@ $CLASS   = __PACKAGE__;
 #    *reply  = \&Email::Reply::reply;
 
 sub reply {
-    my $reply = $CLASS->_new(@_);
-    $reply->_make_headers;
-    $reply->_encapsulate_message if $reply->{attach};
-    $reply->_quote_body($reply->{original})
-      if $reply->{quote} || $reply->{top_post};
-    $reply->_post_reply if $reply->{body};
-    return $reply->{message} ? $reply->_mime : $reply->_simple;
+  my $reply = $CLASS->_new(@_);
+  $reply->_make_headers;
+  $reply->_encapsulate_message if $reply->{attach};
+  $reply->_quote_body($reply->{original})
+    if $reply->{quote} || $reply->{top_post};
+  $reply->_post_reply if $reply->{body};
+  return $reply->{message} ? $reply->_mime : $reply->_simple;
 }
 
 sub _new {
-    my ($class, %args) = @_;
-    my $self = {};
-    $self->{original} = Email::MIME->new(Email::Abstract->as_string($args{to}));
-    $self->{from}     = (
-                          Email::Address->parse($args{from}
-                            || $self->{original}->header('To'))
-                        )[0];
-    $self->{to}       = (Email::Address->parse(
-                          $self->{original}->header('Reply-To')
-                            || $self->{original}->header('From')
-                              || $self->{original}->header('Return-Path')
-                        ))[0];
-    $self->{attrib}   = $args{attrib} || (
-                          ($self->{to}->name || join($self->{to}->address, '<', '>')) .
-                            ' wrote:'
-                        );
-    $self->{prefix}   = $args{prefix} || '> ';
-    $self->{top_post} = $args{top_post};
-    $self->{quote}    = exists $args{quote} ? $args{quote} : 1;
-    $self->{all}      = $args{all};
-    $self->{quoted}   = '';
-    $self->{body}     = $args{body};
-    $self->{attach}   = $args{attach};
-    $self->{keep_sig} = $args{keep_sig};
-    return bless $self, $class;
+  my ($class, %args) = @_;
+  my $self = {};
+  $self->{original} = Email::MIME->new(Email::Abstract->as_string($args{to}));
+
+  ($self->{from}) =
+    Email::Address->parse($args{from} || $self->{original}->header('To'));
+
+  ($self->{to}) = Email::Address->parse(
+       $self->{original}->header('Reply-To')
+    || $self->{original}->header('From')
+    || $self->{original}->header('Return-Path')
+  );
+
+  $self->{attrib} = $args{attrib}
+    || (($self->{to}->name || join($self->{to}->address, '<', '>')) . ' wrote:');
+
+  $self->{prefix}   = $args{prefix} || '> ';
+  $self->{top_post} = $args{top_post};
+  $self->{quote}    = exists $args{quote} ? $args{quote} : 1;
+  $self->{all}      = $args{all};
+  $self->{quoted}   = '';
+  $self->{body}     = $args{body};
+  $self->{attach}   = $args{attach};
+  $self->{keep_sig} = $args{keep_sig};
+
+  return bless $self, $class;
 }
 
 sub _make_headers {
-    my $self = shift;
-    
-    my @header = (
-      From => $self->{from},
-    );
-    
-    $self->{to}->name(
-      (Email::Address->parse($self->{original}->header('From')))[0]->name
-    ) unless $self->{to}->name;
-    push @header, To => $self->{to};
-    
-    my $subject = $self->{original}->header('Subject') || '';
-       $subject = "Re: $subject" unless $subject =~ /\bRe:/i;
-    push @header, Subject => $subject;
+  my $self = shift;
 
-    my ($msg_id) = Email::Address->parse($self->{original}->header('Message-ID'));
-    push @header, 'In-Reply-To' => $msg_id;
-    
-    my @refs = Email::Address->parse($self->{original}->header('References'));
-       @refs = Email::Address->parse($self->{original}->header('In-Reply-To'))
-                 unless @refs;
-    push @refs, $msg_id if $msg_id;
-    push @header, References => join ' ', @refs if @refs;
-    
-    if ( $self->{all} ) {
-        my @addrs = (
-                     Email::Address->parse($self->{original}->header('To')),
-                     Email::Address->parse($self->{original}->header('Cc')),
-                    );
-        unless ( $self->{self} ) {
-            @addrs = grep { $_->address ne $self->{from}->address } @addrs;
-        }
-        push @header, Cc => join ', ', @addrs if @addrs;
+  my @header = (From => $self->{from},);
+
+  $self->{to}
+    ->name((Email::Address->parse($self->{original}->header('From')))[0]->name)
+    unless $self->{to}->name;
+  push @header, To => $self->{to};
+
+  my $subject = $self->{original}->header('Subject') || '';
+  $subject = "Re: $subject" unless $subject =~ /\bRe:/i;
+  push @header, Subject => $subject;
+
+  my ($msg_id) = Email::Address->parse($self->{original}->header('Message-ID'));
+  push @header, 'In-Reply-To' => $msg_id;
+
+  my @refs = Email::Address->parse($self->{original}->header('References'));
+  @refs = Email::Address->parse($self->{original}->header('In-Reply-To'))
+    unless @refs;
+  push @refs, $msg_id if $msg_id;
+  push @header, References => join ' ', @refs if @refs;
+
+  if ($self->{all}) {
+    my @addrs = (
+      Email::Address->parse($self->{original}->header('To')),
+      Email::Address->parse($self->{original}->header('Cc')),
+    );
+    unless ($self->{self}) {
+      @addrs = grep { $_->address ne $self->{from}->address } @addrs;
     }
-    
-    $self->{header} = \@header;
+    push @header, Cc => join ', ', @addrs if @addrs;
+  }
+
+  $self->{header} = \@header;
 }
 
 sub _encapsulate_message {
-    my $self = shift;
-    $self->{message} = Email::MIME->create(
-        attributes => {
-            content_type => 'message/rfc822',
-        },
-        body => $self->{original}->as_string,
-    );
+  my $self = shift;
+  $self->{message} = Email::MIME->create(
+    attributes => { content_type => 'message/rfc822', },
+    body       => $self->{original}->as_string,
+  );
 }
 
 my $crlf = qr/\x0a\x0d|\x0d\x0a|\x0a|\x0d/;
+
 sub _quote_body {
-    my ($self, $part) = @_;
-    return if length $self->{quoted};
-    return map $self->_quote_body($_), $part->parts if $part->parts > 1;
-    return if $part->content_type && $part->content_type !~ m[\btext/plain\b];
+  my ($self, $part) = @_;
+  return if length $self->{quoted};
+  return map $self->_quote_body($_), $part->parts if $part->parts > 1;
+  return if $part->content_type && $part->content_type !~ m[\btext/plain\b];
 
-    my $body = $part->body;
+  my $body = $part->body;
 
-    $body = ($self->_strip_sig($body) || $body)
-      if ! $self->{keep_sig} && $body =~ /$crlf--\s*$crlf/o;
+  $body = ($self->_strip_sig($body) || $body)
+    if !$self->{keep_sig} && $body =~ /$crlf--\s*$crlf/o;
 
-    my ($end) = $body =~ /($crlf)/; $end ||= "\r\n";
-    $body =~ s/[\r\n\s]+$//;
-    $body =  $self->_quote_orig_body($body);
-    $body =  "$self->{attrib}$end$body$end";
+  my ($end) = $body =~ /($crlf)/;
+  $end ||= $CRLF;
+  $body =~ s/[\r\n\s]+$//;
+  $body = $self->_quote_orig_body($body);
+  $body = "$self->{attrib}$end$body$end";
 
-    $self->{crlf} = $end;
-    $self->{quoted} = $body;
+  $self->{crlf}   = $end;
+  $self->{quoted} = $body;
 }
 
 # Yes, you are witnessing elitism.
 sub _strip_sig { reverse +(split /$crlf\s*--$crlf/o, reverse(pop), 2)[1] }
 
 sub _quote_orig_body {
-    my ($self, $body) = @_;
-    $body =~ s/($crlf)/$1$self->{prefix}/g;
-    "$self->{prefix}$body";
+  my ($self, $body) = @_;
+  $body =~ s/($crlf)/$1$self->{prefix}/g;
+  "$self->{prefix}$body";
 }
 
 sub _post_reply {
-    my $self = shift;
-    return $self->{reply_body} = $self->{body}
-      unless length $self->{quoted};
-    my @parts = (@{$self}{qw[quoted body]});
-       @parts = reverse @parts if $self->{top_post};
-    $self->{reply_body} = join $self->{crlf}, @parts;
+  my $self = shift;
+  return $self->{reply_body} = $self->{body}
+    unless length $self->{quoted};
+  my @parts = (@{$self}{qw[quoted body]});
+  @parts = reverse @parts if $self->{top_post};
+  $self->{reply_body} = join $self->{crlf}, @parts;
 }
 
 sub _mime {
-    my $self = shift;
-    Email::MIME->create(
-        header => $self->{header},
-        parts  => [
-                   Email::MIME->create(body => $self->{reply_body}),
-                   $self->{message},
-                  ],
-    );
+  my $self = shift;
+  Email::MIME->create(
+    header => $self->{header},
+    parts =>
+      [ Email::MIME->create(body => $self->{reply_body}), $self->{message}, ],
+  );
 }
 
 sub _simple {
-    my $self = shift;
-    Email::Simple->create(
-        header => $self->{header},
-        body   => $self->{reply_body},
-    );
+  my $self = shift;
+  Email::Simple->create(
+    header => $self->{header},
+    body   => $self->{reply_body},
+  );
 }
 
 1;
